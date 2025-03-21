5a6f7735e110fd469e12891cfb40b4b1d1fb6e05
Mohak3345 <mohak3345@gmail.com>
2025-03-21 23:53:50 +0530

    psot notes added

--- notes-aid/src/notes/sy/it/evenSem/psot/data.tsx
+++ notes-aid/src/notes/sy/it/evenSem/psot/data.tsx
@@ -6,9 +6,25 @@ const psot = {
   color: "blue",
   modules: {
     1: {
-      notesLink: [],
-      topics: [],
+      notesLink: ["https://drive.google.com/drive/folders/1pWfvzh36fTVWtxyrvGlbtmn4bxb4R"],
+      topics: ["Probability and Probability Distribution"],
     },
+    2: {
+      notesLink: ["https://drive.google.com/drive/folders/1fgdxBEi-FXT5H8I4H9p1XkqFRTfJkS9H"],
+      topics: ["Correlation and Regression"],
+    },
+    3: {
+      notesLink: ["https://drive.google.com/drive/folders/1_03yp27noK0QV4PSQTYuPICkwWVNcBkd"],
+      topics: ["Estimation and Sampling Theory"],
+    },
+    4: {
+      notesLink: ["https://drive.google.com/drive/folders/1oex-96QHpyOLydudYsBMyubZdhvQciFf"],
+      topics: ["Optimization"],
+    },
+    5: {
+      notesLink: ["https://drive.google.com/drive/folders/1qGkqSoqRohL3_A5oGufk6tjGPNObQzRv"],
+      topics: ["Queuing Theory"],
+    }
   },
 };
 
