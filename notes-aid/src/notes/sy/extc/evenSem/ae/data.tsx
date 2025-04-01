aeac956956aba533979106a4135de617843f30a5
Divay Galani <134092426+divaygalani@users.noreply.github.com>
2025-04-01 23:26:32 +0530

    Update data.tsx

--- notes-aid/src/notes/sy/extc/evenSem/ae/data.tsx
+++ notes-aid/src/notes/sy/extc/evenSem/ae/data.tsx
@@ -1,19 +1,72 @@
-import { Brain } from "lucide-react"; 
-
+import { Brain } from "lucide-react";
 
 const ae = {
   name: "Analog Electronics",
   icon: Brain,
   color: "blue",
   modules: {
-   1:{
-    notesLink:[
-
-    ],
-    topics:[
-
-    ]
-   }
+    1: {
+      title: "Frequency Response of BJT and MOSFET Amplifiers",
+      notesLink: [
+        {
+          title: "Swati ma'am ppt",
+          url: "https://drive.google.com/file/d/1-XKAB9rSpbKxcUMzJzSJvZ5F8q4UnnsT/view?usp=sharing",
+        },
+        {
+          title: "Best for AC analysis of BJT & MOSFET low & high frequency (also good for many concepts of mod 1, match with syllabus & see)",
+          url: "https://www.youtube.com/embed/videoseries?si=6wqruKIsoxNDkUET&list=PLBDB2c4Mp7hAcV9zW7SGbTd7QDXrhG6nY",
+        },
+      ],
+    },
+    2: {
+      title: "Biasing Techniques for Integrated Circuits",
+      notesLink: [
+        {
+          title: "Swati ma'am ppt",
+          url: "https://drive.google.com/file/d/1-b50blaDrskeHAJpFo4YIEBWm4Y5nEuO/view?usp=sharing",
+        },
+      ],
+    },
+    3: {
+      title: "Fundamentals of Operational Amplifier",
+      notesLink: [
+        {
+          title: "Operational amplifier BEST playlist 3.3 & 3.4",
+          url: "https://www.youtube.com/embed/videoseries?si=-7i_PXDYxivGnxtx&list=PLS025GDZpC8y-D11rEFzWtj35uuTOqLNY",
+        },
+      ],
+    },
+    4: {
+      title: "Power Amplifier",
+      notesLink: [
+        {
+          title: "BEST YT",
+          url: "https://www.youtube.com/embed/videoseries?si=VRIswP9hQX8UBfdL&list=PLdEuLNelwfBPCzQH7PugcsHw6jnDfROwO",
+        },
+      ],
+    },
+    5: {
+      title: "Oscillator Circuits",
+      notesLink: [],
+    },
+    lab_experiments: {
+      title: "Lab Experiments",
+      notesLink: [
+        {
+          title: "Lab Experiments",
+          url: "https://drive.google.com/drive/folders/13eUBq-YizHfSQHEj_X_AMZIHHA1AiFfe?usp=sharing",
+        },
+      ],
+    },
+    senior_notes: {
+      title: "Senior Notes",
+      notesLink: [
+        {
+          title: "Senior Notes",
+          url: "https://drive.google.com/drive/folders/1EYBh3LhyrifTMSArm_4Go98BKvqhKEbW?usp=sharing",
+        },
+      ],
+    },
   },
 };
 
