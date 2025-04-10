1e64a4d1472f1bf11534ac3697ddbc589d3ab678
chaurasia-aryan <chaurasiaaryan1980@gmail.com>
2025-04-10 14:57:30 +0530

    Final Ep Notes and Videos

--- notes-aid/src/notes/fy/comps/evenSem/ep/data.tsx
+++ notes-aid/src/notes/fy/comps/evenSem/ep/data.tsx
@@ -1,4 +1,4 @@
-import { Brain } from "lucide-react"; 
+import { Brain } from "lucide-react";
 
 const ep = {
   name: "Engineering Physics",
@@ -17,10 +17,22 @@ const ep = {
           title: "1.1 Lasers",
           description: "Easy to cover topic",
           videos: [
+
             {
-              title: "Laser Playlist",
-              url: "https://www.youtube.com/watch?v=MfQCav-N24A&list=PLQzUXa8lZVq8flXCtrKZ7e9vFfgpynWqz"
-            }
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/wOyVaF-DG4A?si=ixsPvtf461h-tjz8"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/eQGVkFBjGzE?si=kmA1gOExdo5Rl0qB"
+            },
+            {
+              title: "Part 3",
+
+              url: "https://www.youtube.com/embed/He-ygEP8ifo?si=CFZXivNojDHAM-Ep"
+            },
+
+
           ]
         },
         {
@@ -28,8 +40,8 @@ const ep = {
           description: "Easy to cover topic",
           videos: [
             {
-              title: "Optical Fibers Playlist",
-              url: "https://www.youtube.com/playlist?list=PLQzUXa8lZVq8c5fWjeSLsUaKu_BmcKL2A"
+              title: "Optical Fibers",
+              url: "https://www.youtube.com/embed/ntTjTsiZ478?si=aVMUpEu6EJXrIv12"
             }
           ]
         }
@@ -48,8 +60,28 @@ const ep = {
           description: "Bit complex to understand",
           videos: [
             {
-              title: "Semiconductor Playlist",
-              url: "https://www.youtube.com/playlist?list=PLQzUXa8lZVq8lfZN5836sohUpRCt2b6oB"
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/CXFtbpYaBXw"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/6ShYjai83bA"
+            },
+            {
+              title: "Part 3",
+              url: "https://www.youtube.com/embed/67QXoPW0eE0"
+            },
+            {
+              title: "Part 4",
+              url: "https://www.youtube.com/embed/5iEsqeMOZAc"
+            },
+            {
+              title: "Part 5",
+              url: "https://www.youtube.com/embed/Z-mdnY2ObRg"
+            },
+            {
+              title: "Part 6",
+              url: "https://www.youtube.com/embed/85KspLpuoXU"
             }
           ]
         },
@@ -57,19 +89,58 @@ const ep = {
           title: "2.2 Dielectric",
           description: "Quite Lengthy theory Focus on numericals",
           videos: [
+
             {
-              title: "Dielectric Playlist",
-              url: "https://www.youtube.com/playlist?list=PLAPKGqvQGg6rR96A5REMgwkeacPOhn83X"
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/ZC4GgMapjHo"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/MsDfu0kh14Q"
+            },
+            {
+              title: "Part 3",
+              url: "https://www.youtube.com/embed/Idq5jiO1qSA"
+            },
+            {
+              title: "Part 4",
+              url: "https://www.youtube.com/embed/3b8YoeR9ew8"
+            },
+            {
+              title: "Part 5",
+              url: "https://www.youtube.com/embed/3pCcixzTgws"
+            },
+            {
+              title: "Part 6",
+              url: "https://www.youtube.com/embed/ZNcFK2ky6uc"
+            },
+            {
+              title: "Part 7",
+              url: "https://www.youtube.com/embed/k-iwlRzvpOE"
+            },
+            {
+              title: "Part 8",
+              url: "https://www.youtube.com/embed/UUPSBh5NmSU"
+            },
+            {
+              title: "Part 9",
+              url: "https://www.youtube.com/embed/-LB5cnuGGTs"
+            },
+            {
+              title: "Part 10",
+              url: "https://www.youtube.com/embed/y-RH0iT_p2U"
             }
+
+
           ]
         },
         {
           title: "2.3 Liquid Crystal",
-          description: "Easy to cover topic",
+          description: "Easy to cover topic, But do attend the online lecture taken by faculty for this",
           videos: [
             {
-              title: "Liquid Crystal Playlist",
-              url: "https://www.youtube.com/playlist?list=PLQzUXa8lZVq-GpWcleSQCxu1pczvgWTgJ"
+              title: "Liquid Crystal",
+              url: "https://www.youtube.com/embed/_2z-uWRkXh8?si=ZuwI9xiKcTETXkHH"
             }
           ]
         }
@@ -87,10 +158,37 @@ const ep = {
           title: "3.1 Quantum Mechanics",
           description: "Needs Some Foundational basics to understand, bit hard though",
           videos: [
+
             {
-              title: "Quantum Mechanics Playlist",
-              url: "https://www.youtube.com/playlist?list=PLQzUXa8lZVq8Zciw0MsOdPJ1t-bpPKV6_"
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/R3YIlcCZWxQ"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/TBDrutCvKN4"
+            },
+            {
+              title: "Part 3",
+              url: "https://www.youtube.com/embed/nC_OAaG7T50"
+            },
+            {
+              title: "Part 4",
+              url: "https://www.youtube.com/embed/caAPH4RT_Ec"
+            },
+            {
+              title: "Part 5",
+              url: "https://www.youtube.com/embed/P6YnCbvvvHY"
+            },
+            {
+              title: "Part 6",
+              url: "https://www.youtube.com/embed/EtNBK8uoFDY"
+            },
+            {
+              title: "Part 7",
+              url: "https://www.youtube.com/embed/4Lvnk0FwHEo"
             }
+
+
           ]
         }
       ]
@@ -105,8 +203,25 @@ const ep = {
       topics: [
         {
           title: "4.1 Electromagnetism and Introduction to Plasma Physics",
-          description: "Bit complex and Lengthy",
-          videos: []
+          description: "Bit complex and Lengthy, Focus on Derivations from this",
+          videos: [
+
+
+            {
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/cWZSCK2UiZc"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/UsZHdfpQZkA"
+            },
+            {
+              title: "Part 3",
+              url: "https://www.youtube.com/embed/Npr77Hg_Tmw"
+            }
+
+
+          ]
         }
       ]
     },
@@ -122,10 +237,26 @@ const ep = {
           title: "5.1 Physics of Sensors for IoT Applications",
           description: "Easy to cover topic",
           videos: [
+
+
             {
-              title: "IoT Sensors Playlist",
-              url: "https://www.youtube.com/playlist?list=PL91lquAVmESBqKLU0Tn5gRVXVyW5KLgCa"
+              title: "Part 1",
+              url: "https://www.youtube.com/embed/WbeR0Ey3qPA"
+            },
+            {
+              title: "Part 2",
+              url: "https://www.youtube.com/embed/dFIk4nGEREg"
+            },
+            {
+              title: "Part 3",
+              url: "https://www.youtube.com/embed/kCbZW6b9FKc"
+            },
+            {
+              title: "Part 4",
+              url: "https://www.youtube.com/embed/of3-ibcfrdk"
             }
+
+
           ]
         }
       ]
