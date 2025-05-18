37748c09ee7082e367bd54dd4e2b2b8a65bb3eb2
Samridha Das <samridha2020@gmail.com>
2025-05-18 22:26:07 +0530

    data.tsx
    AM-I notes uploaded

--- notes-aid/src/notes/fy/comps/oddSem/am1/data.tsx
+++ notes-aid/src/notes/fy/comps/oddSem/am1/data.tsx
@@ -6,10 +6,50 @@ const am1 = {
   color: "blue",
   modules: {
     1: {
-      notesLink: [],
+      notesLink: [
+        {
+          title: "MOD-1 Notes",
+          url: "https://drive.google.com/drive/folders/1Eikm_-bjyrV8n9d0UeNq-GvI1_KdnL95?usp=sharing",
+        },
+      ],
       topics: [],
     },
-  },
+    2: {
+      notesLink: [
+        {
+          title: "MOD-2 Notes",
+          url: "https://drive.google.com/drive/folders/16BMER-kmLog1enTHb-Yt2Ti_wjwIVB8-?usp=sharing",
+        },
+      ],
+      topics: [],
+    },
+    3: {
+      notesLink: [
+        {
+          title: "MOD-3 Notes",
+          url: "https://drive.google.com/drive/folders/16iFeqFgYJJqeOSq-la5j0KlVSjMT3eNw?usp=sharing",
+        },
+      ],
+      topics: [],
+    },
+    4: {
+      notesLink: [
+        {
+          title: "MOD-4 Notes",
+          url: "https://drive.google.com/drive/folders/1kOU08yQ8Qv5CDCgsG-yXQVLFj9wdb2Rc?usp=sharing",
+        },
+      ],
+      topics: [],
+    },
+    5: {
+      notesLink: [
+        {
+          title: "MOD-5 Notes",
+          url: "https://drive.google.com/drive/folders/1yzaGKBMGBxCKerHjnodvoxgY36ET90Zr?usp=sharing",
+        },
+      ],
+      topics: [],
+    }, 
 };
 
 export default am1;
